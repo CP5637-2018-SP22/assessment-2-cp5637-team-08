be3bf3e4d855f13883ad541f63947a4099382306
Maikal Shrestha <shrestha.maikal007@gmail.com>
2018-09-14 10:59:26 +1000

    updated theme, headers, navigations

--- themes/u3atheme/comments.php
+++ themes/u3atheme/comments.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * The template for displaying comments
+ * The template for displaying comments.
  *
  * This is the template that displays the area of the page that contains both the current comments
  * and the comment form.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package u3atheme
  */
@@ -24,50 +24,49 @@ if ( post_password_required() ) {
 
 	<?php
 	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
+	if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-			$u3atheme_comment_count = get_comments_number();
-			if ( '1' === $u3atheme_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'u3atheme' ),
-					'<span>' . get_the_title() . '</span>'
-				);
-			} else {
 				printf( // WPCS: XSS OK.
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $u3atheme_comment_count, 'comments title', 'u3atheme' ) ),
-					number_format_i18n( $u3atheme_comment_count ),
+					esc_html( _nx( 'One comment', '%1$s comments', get_comments_number(), 'comments title', 'u3atheme' ) ),
+					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
 				);
-			}
 			?>
-		</h2><!-- .comments-title -->
-
-		<?php the_comments_navigation(); ?>
+		</h2>
 
 		<ol class="comment-list">
 			<?php
-			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
-			) );
+				wp_list_comments( array(
+					'style'      => 'ol',
+					'short_ping' => true,
+					'avatar_size'=> '96',
+				) );
 			?>
 		</ol><!-- .comment-list -->
 
-		<?php
-		the_comments_navigation();
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'u3atheme' ); ?></h2>
+			<div class="nav-links">
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'u3atheme' ); ?></p>
-			<?php
-		endif;
+				<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'u3atheme' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'u3atheme' ) ); ?></div>
+
+			</div><!-- .nav-links -->
+		</nav><!-- #comment-nav-below -->
+		<?php
+		endif; // Check for comment navigation.
 
 	endif; // Check for have_comments().
+
+
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'u3atheme' ); ?></p>
+	<?php
+	endif;
 
 	comment_form();
 	?>
--- themes/u3atheme/functions.php
+++ themes/u3atheme/functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * u3atheme functions and definitions
+ * u3atheme functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
@@ -8,168 +8,116 @@
  */
 
 if ( ! function_exists( 'u3atheme_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function u3atheme_setup() {
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on u3atheme, use a find and replace
+	 * to change 'u3atheme' to the name of your theme in all the template files.
 	 */
-	function u3atheme_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on u3atheme, use a find and replace
-		 * to change 'u3atheme' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'u3atheme', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'u3atheme', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+	add_image_size( 'u3atheme-full-bleed', 2000, 1200, true );
+	add_image_size( 'u3atheme-index-img', 1000, 550, true );
 
-		// This theme uses wp_nav_menu() in one location.
-		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'u3atheme' ),
-		) );
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => esc_html__( 'Header', 'u3atheme' ),
+		'social' => esc_html__( 'Social Media Menu', 'u3atheme' ),
+	) );
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( 'html5', array(
+		'search-form',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'caption',
+	) );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'u3atheme_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+	// Set up the WordPress core custom background feature.
+	add_theme_support( 'custom-background', apply_filters( 'u3atheme_custom_background_args', array(
+		'default-color' => 'ffffff',
+		'default-image' => '',
+	) ) );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+	// Add theme support for Custom Logo
+	add_theme_support( 'custom-logo', array(
+		'width' => 90,
+		'height' => 90,
+		'flex-width' => true,
+	));
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
+	/* Editor styles */
+	add_editor_style( array( 'inc/editor-styles.css', u3atheme_fonts_url() ) );
+}
 endif;
 add_action( 'after_setup_theme', 'u3atheme_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function u3atheme_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'u3atheme_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'u3atheme_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function u3atheme_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'u3atheme' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'u3atheme' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'u3atheme_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function u3atheme_scripts() {
-    wp_enqueue_style('u3atheme-fonts', u3atheme_fonts_url());
-    
-	wp_enqueue_style( 'u3atheme-style', get_stylesheet_uri() );
-
-	//wp_enqueue_style( 'u3atheme-fontawesome', 'https://use.fontawesome.com/releases/v5.3.1/css/all.css' );
-
-	//wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
-
-	wp_enqueue_script( 'u3atheme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'u3atheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'u3atheme_scripts' );
 
 /**
  * Register custom fonts.
  */
 function u3atheme_fonts_url() {
-    $fonts_url = '';
+	$fonts_url = '';
 
-    /*
-     * Translators: If there are characters in your language that are not
-     * supported by Lato and MPLUS, translate this to 'off'. Do not translate
-     * into your own language.
-     */
-    $merriweather = _x( 'on', 'Merriweather font: on or off', 'u3atheme' );
-    $roboto = _x( 'on', 'Roboto font: on or off', 'u3atheme' );
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'u3atheme' );
+	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'u3atheme' );
 
-    $font_families = array();
-    if ( 'off' !== $merriweather ) {
-        $font_families[] = 'Merriweather:400,700';
-    }
-    if ('off' !== $roboto) {
-        $font_families[] = 'Roboto:400,400i,500,500i';
-    }
+	$font_families = array();
 
-    if (in_array('on', array($merriweather, $roboto))) {
-        $query_args = array(
-            'family' => urlencode( implode( '|', $font_families ) ),
-            'subset' => urlencode( 'latin,latin-ext' ),
-        );
+	if ( 'off' !== $source_sans_pro ) {
+		$font_families[] = 'Source Sans Pro:400,400i,700,900';
+	}
 
-        $fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
-    }
+	if ( 'off' !== $pt_serif ) {
+		$font_families[] = 'PT Serif:400,400i,700,700i';
+	}
 
-    return esc_url_raw( $fonts_url );
+
+	if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
 }
 
 /**
@@ -182,17 +130,167 @@ function u3atheme_fonts_url() {
  * @return array $urls           URLs to print for resource hints.
  */
 function u3atheme_resource_hints( $urls, $relation_type ) {
-    if ( wp_style_is( 'u3atheme-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
-        $urls[] = array(
-            'href' => 'https://fonts.gstatic.com',
-            'crossorigin',
-        );
-    }
+	if ( wp_style_is( 'u3atheme-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
 
-    return $urls;
+	return $urls;
 }
 add_filter( 'wp_resource_hints', 'u3atheme_resource_hints', 10, 2 );
 
+/**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function u3atheme_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'u3atheme_content_width', 640 );
+}
+add_action( 'after_setup_theme', 'u3atheme_content_width', 0 );
+
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for content images.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $sizes A source size value for use in a 'sizes' attribute.
+ * @param array  $size  Image size. Accepts an array of width and height
+ *                      values in pixels (in that order).
+ * @return string A source size value for use in a content image 'sizes' attribute.
+ */
+function u3atheme_content_image_sizes_attr( $sizes, $size ) {
+	$width = $size[0];
+
+	if ( 900 <= $width ) {
+		$sizes = '(min-width: 900px) 700px, 900px';
+	}
+
+	if ( is_active_sidebar( 'sidebar-1' ) || is_active_sidebar( 'sidebar-2' ) ) {
+		$sizes = '(min-width: 900px) 600px, 900px';
+	}
+
+	return $sizes;
+}
+add_filter( 'wp_calculate_image_sizes', 'u3atheme_content_image_sizes_attr', 10, 2 );
+
+/**
+ * Filter the `sizes` value in the header image markup.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param string $html   The HTML image tag markup being filtered.
+ * @param object $header The custom header object returned by 'get_custom_header()'.
+ * @param array  $attr   Array of the attributes for the image tag.
+ * @return string The filtered header image HTML.
+ */
+function u3atheme_header_image_tag( $html, $header, $attr ) {
+	if ( isset( $attr['sizes'] ) ) {
+		$html = str_replace( $attr['sizes'], '100vw', $html );
+	}
+	return $html;
+}
+add_filter( 'get_header_image_tag', 'u3atheme_header_image_tag', 10, 3 );
+
+/**
+ * Add custom image sizes attribute to enhance responsive image functionality
+ * for post thumbnails.
+ *
+ * @origin Twenty Seventeen 1.0
+ *
+ * @param array $attr       Attributes for the image markup.
+ * @param int   $attachment Image attachment ID.
+ * @param array $size       Registered image size or flat array of height and width dimensions.
+ * @return string A source size value for use in a post thumbnail 'sizes' attribute.
+ */
+function u3atheme_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
+
+	if ( !is_singular() ) {
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			$attr['sizes'] = '(max-width: 900px) 90vw, 800px';
+		} else {
+			$attr['sizes'] = '(max-width: 1000px) 90vw, 1000px';
+		}
+	} else {
+		$attr['sizes'] = '100vw';
+	}
+
+	return $attr;
+}
+add_filter( 'wp_get_attachment_image_attributes', 'u3atheme_post_thumbnail_sizes_attr', 10, 3 );
+
+
+
+
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function u3atheme_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'u3atheme' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add sidebar widgets here.', 'u3atheme' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Page Sidebar', 'u3atheme' ),
+		'id'            => 'sidebar-2',
+		'description'   => esc_html__( 'Add page sidebar widgets here.', 'u3atheme' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widgets', 'u3atheme' ),
+		'id'            => 'footer-1',
+		'description'   => esc_html__( 'Add footer widgets here.', 'u3atheme' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+}
+add_action( 'widgets_init', 'u3atheme_widgets_init' );
+
+/**
+ * Enqueue scripts and styles.
+ */
+function u3atheme_scripts() {
+	// Enqueue Google Fonts: Source Sans Pro and PT Serif
+	wp_enqueue_style( 'u3atheme-fonts', u3atheme_fonts_url() );
+
+	wp_enqueue_style( 'u3atheme-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'u3atheme-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
+	wp_localize_script( 'u3atheme-navigation', 'u3athemeScreenReaderText', array(
+		'expand' => __( 'Expand child menu', 'u3atheme'),
+		'collapse' => __( 'Collapse child menu', 'u3atheme'),
+	));
+
+	wp_enqueue_script( 'u3atheme-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20161201', true );
+
+	wp_enqueue_script( 'u3atheme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'u3atheme_scripts' );
 
 /**
  * Implement the Custom Header feature.
@@ -205,9 +303,9 @@ require get_template_directory() . '/inc/custom-header.php';
 require get_template_directory() . '/inc/template-tags.php';
 
 /**
- * Functions which enhance the theme by hooking into WordPress.
+ * Custom functions that act independently of the theme templates.
  */
-require get_template_directory() . '/inc/template-functions.php';
+require get_template_directory() . '/inc/extras.php';
 
 /**
  * Customizer additions.
@@ -217,7 +315,9 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
+require get_template_directory() . '/inc/jetpack.php';
 
+/**
+ * Load SVG icon functions.
+ */
+require get_template_directory() . '/inc/icon-functions.php';
